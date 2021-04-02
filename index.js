d6700391c81fc91a71b9348f288962ebb897b0e8
Jan Richie Yu <yujanrichie@gmail.com>
2021-04-03 03:17:58 +0800

    create initial CartManager object

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-//code starts here for cart manager
+function CartManager() {
+    let cartItems = [];
+
+    this.storeItems = function() { };
+
+    this.pushItems = function() { };
+
+    this.updateItems = function() { };
+
+    this.removeItems = function() { };
+
+    this.subscribeCartUpdates = function(callbackFunction) { };
+
+    //readonly cartItems property
+    Object.defineProperty(this, 'cartItems', {
+        get: function() {
+            return cartItems;
+        }
+    });
+}
